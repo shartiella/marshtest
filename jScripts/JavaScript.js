3dcde1c298e7d9b0f06c94f9148d86da86467ab3
shartiella <82021382+shartiella@users.noreply.github.com>
2022-01-28 19:02:13 +0200

    עם HTML

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,47 @@
+﻿//var buttonCount = 0;
+
+//function openTab(evt, chosenTab) {
+//    var i, tabs, tablinks;
+//    tabs = document.getElementsByClassName("tab");
+//    for (i = 0; i < tabs.length; i++) {
+//        tabs[i].style.display = "none";
+//    }
+
+//    tablinks = document.getElementsByClassName("tablink");
+
+//    for (i = 0; i < tablinks.length; i++) {
+//                tablinks[i].className = tablinks[i].className.replace(" checked", "");
+//    }
+//    document.getElementById(chosenTab).style.display = "block";
+//    evt.currentTarget.className += " checked";
+//}
+
+//function stopani() {
+//    console.log(buttonCount);
+
+//    buttonCount++;
+
+//    if (buttonCount % 2 != 0) {
+//        document.getElementById("matswitch").style.backgroundImage = "url('/images/switchgifstill.png')";
+//        document.getElementById("stopani").value = "הפעלת אנימציה";
+//    }
+//    else {
+//        document.getElementById("matswitch").style.backgroundImage = "url('/images/switchgif.gif')";
+//        document.getElementById("stopani").value = "עצירת אנימציה";
+//    }
+//}
+
+function openAbout() {
+    document.getElementById("aboutPopUp").classList.remove('aboutPopUpClosed');
+    document.getElementById("aboutPopUp").classList.add('aboutPopUpOpen');
+    document.getElementById("aboutPopUpX").classList.remove('aboutPopUpXclosed');
+    document.getElementById("aboutPopUpX").classList.add('aboutPopUpXopen');
+
+}
+
+function closeAbout() {
+    document.getElementById("aboutPopUp").classList.remove('aboutPopUpOpen');
+    document.getElementById("aboutPopUp").classList.add('aboutPopUpClosed');
+    document.getElementById("aboutPopUpX").classList.remove('aboutPopUpXopen');
+    document.getElementById("aboutPopUpX").classList.add('aboutPopUpXclosed');
+}
